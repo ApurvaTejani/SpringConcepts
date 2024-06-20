01d0c9b3db516b9b7f8d69a82da482ee078497eb
ApurvaTejani <aputejacc@gmail.com>
2024-06-20 20:54:59 +0530

    Two rest endpoints with addition of spring boot devtools

--- myfirstapp/src/main/java/com/firstproject/demo/myapp/rest/MyAppRestController.java
+++ myfirstapp/src/main/java/com/firstproject/demo/myapp/rest/MyAppRestController.java
@@ -1,13 +1,21 @@
 package com.firstproject.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyAppRestController {
+
+    @Value("${first.name}")
+    private String firstName;
     @GetMapping
     public String sayHello(){
+        return firstName+" Tejani";
+        }
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run daily and stay fit";
 
-        return "Hello People";
     }
 }
